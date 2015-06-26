71166c6146efd7f6375de8c2a0a9c32d0624f733
Egor Vakhromtsev <e.vakhromtsev@mediacrat.com>
2015-06-26 16:53:35 +0300

    Output to svg instead png

--- lib/aasm/graph.rb
+++ lib/aasm/graph.rb
@@ -5,12 +5,12 @@ module AASM
     def initialize(class_name, options = {})
       options = {path: '.'}.merge(options)
 
-      @file_path = File.join(options[:path], "#{class_name.parameterize('_')}_aasm.png")
+      @file_path = File.join(options[:path], "#{class_name.parameterize('_')}_aasm.svg")
       super(:G)
     end
 
     def output
-      super png: @file_path
+      super svg: @file_path
     end
   end
 end
